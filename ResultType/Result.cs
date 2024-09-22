ab7bb7430f0583c29ea37f108925ec8905965814
m1cr0waveoven <51439076+m1cr0waveoven@users.noreply.github.com>
2024-09-22 22:01:38 +0200

    Added implicit operators and Match function to the Result class

--- ResultType/Result.cs
+++ ResultType/Result.cs
@@ -7,11 +7,17 @@ public class Result<TValue>
 
     private readonly bool _isSucces;
 
-    private Result(TValue value) => (_value, _isSucces) = (value, true);
+    private Result(TValue value) => (_value, _isSucces, _error) = (value, true, Error.None);
 
     private Result(Error error) => (_error, _isSucces) = (error, false);
 
     public static Result<TValue> Success(TValue value) => new(value);
 
     public static Result<TValue> Failure(Error error) => new(error);
+
+    public static implicit operator Result<TValue>(TValue value) => new(value);
+    
+    public static implicit operator Result<TValue>(Error error) => new(error);
+
+    public TResult Match<TResult>(Func<TValue, TResult> success, Func<Error, TResult> failure) => _isSucces ? success(_value!) : failure(_error);
 }
