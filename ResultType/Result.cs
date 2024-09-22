5903b0513c36dba9d7ecdda088763eb89cf5708f
m1cr0waveoven <51439076+m1cr0waveoven@users.noreply.github.com>
2024-09-22 11:09:58 +0200

    Add project files.

---
+++ ResultType/Result.cs
@@ -0,0 +1,17 @@
+﻿namespace ResultType;
+
+public class Result<TValue>
+{
+    private readonly TValue? _value;
+    private readonly Error _error;
+
+    private readonly bool _isSucces;
+
+    private Result(TValue value) => (_value, _isSucces) = (value, true);
+
+    private Result(Error error) => (_error, _isSucces) = (error, false);
+
+    public static Result<TValue> Success(TValue value) => new(value);
+
+    public static Result<TValue> Failure(Error error) => new(error);
+}
